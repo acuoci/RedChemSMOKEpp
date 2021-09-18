3ccdb35e065dacf5f581c131f2a2880ea0514b7f
Alberto Cuoci <alberto.cuoci@polimi.it>
2021-09-18 13:32:44 +0200

    Modified setup options

--- examples/example_01/Run.bat
+++ examples/example_01/Run.bat
@@ -1,2 +1,2 @@
-"C:\Users\acuoci\OneDrive - Politecnico di Milano\My Projects\RedChemSMOKE++\exe\windows\MSVS2019\x64\Release\RedChemSMOKE++.exe" --input input.dic
+"RedChemSMOKE++.exe" --input input.dic
 pause
